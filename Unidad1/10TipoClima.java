e036fe15f7295bfe8737162a4456ca39615c0a21
AdolfoQuirozRobles <joseadolfoqr94@gmail.com>
2023-06-13 14:57:16 -0600

    se agrega tipo de clima

---
+++ Unidad1/10TipoClima.java
@@ -0,0 +1,24 @@
+package pkg10tipoclima;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Ingrese la temperatura en grados centígrados: ");
+        double temperatura = scanner.nextDouble();
+
+        if (temperatura < 10) {
+            System.out.println("El clima es frío.");
+        } else if (temperatura >= 10 && temperatura < 20) {
+            System.out.println("El clima está nublado.");
+        } else if (temperatura >= 20 && temperatura < 30) {
+            System.out.println("El clima es caluroso.");
+        } else {
+            System.out.println("El clima es tropical.");
+        }
+    }
+}
+ 
+    
+
