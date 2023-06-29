c2c7a4e694e512fc0de138b9f610e2d5840022d3
AdolfoQuirozRobles <joseadolfoqr94@gmail.com>
2023-06-29 12:27:29 -0600

    se agrega media

---
+++ unidad2/2-9WHILEMEDIA.java
@@ -0,0 +1,27 @@
+
+package pkg2.pkg9whilemedia;
+import java.util.Scanner;
+public class Main {
+
+    
+    public static void main(String[] args) {
+        Scanner entrada=new Scanner(System.in);
+        int num,suma,elementos;
+        float media;
+        System.out.println("Introduzca un número");
+        num=entrada.nextInt();
+        suma=0;
+        elementos=0;
+        while(num>=0){
+            suma+=num;
+            elementos++;
+            System.out.println("Introduce otro numero: ");
+            num=entrada.nextInt();
+        }
+                media=(float)suma/elementos;
+                System.out.println("La media es de: "+media);
+            }
+        }
+    
+    
+
